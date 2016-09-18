15e009985d9a92b04cf22f5e4735d879d81425dc
qiaozhen001 <215504265@qq.com>
2016-09-18 23:08:56 +0800

    洞主救我

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 var app = require('express')(),
-    redisClient = require('./startup/startup-redis.js'),
-    mysqlClient = require('./startup/startup-mysql.js'),
+    //redisClient = require('./startup/startup-redis.js'),
+    //mysqlClient = require('./startup/startup-mysql.js'),
     mongodbClient = require('./startup/startup-mongodb.js'),
     startupExpress = require('./startup/startup-express.js')(app),
     startupRouter = require('./startup/startup-router.js')(app);
