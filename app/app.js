95e8c801b7ecad326ccb17f04e3831301a5eeaaa
darkmatterjinx <darkmatter@qq.com>
2016-09-18 21:17:32 +0800

    index.jade

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 var app = require('express')(),
-    //redisClient = require('./startup/startup-redis.js'),
-    //mysqlClient = require('./startup/startup-mysql.js'),
+    redisClient = require('./startup/startup-redis.js'),
+    mysqlClient = require('./startup/startup-mysql.js'),
     mongodbClient = require('./startup/startup-mongodb.js'),
     startupExpress = require('./startup/startup-express.js')(app),
     startupRouter = require('./startup/startup-router.js')(app);
 
 var socketIo = require('./startup/startup-socketIo.js')(app);
+
+
+var Movie = require('./dao/mongodb/model/Movie.js');
 
 app.get('/socket-io', function(req, res) {
     res.render('./demo/socket-io.jade');
@@ -13,5 +16,8 @@ app.get('/socket-io', function(req, res) {
 
 //乔test
 app.get('/', function(req, res) {
-    res.render('./page/index.jade');
+    Movie.listByPagination(0,10,function(err,movies){
+        if(err) console.log(err);
+        res.render('./page/index.jade',{movies:movies});
+    });
 });
